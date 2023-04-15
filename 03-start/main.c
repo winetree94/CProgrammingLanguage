7ee1192bd1c6be732970be8e5e0e01fe5fec4b5e
winetree94 <winetree94@gmail.com>
2023-04-15 17:34:17 +0900

    update

---
+++ 03-start/main.c
@@ -0,0 +1,11 @@
+// boolean 사용
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("Hello world!");
+  bool b = true;
+  float c = 1.0;
+  int d = 1.0;
+  return 0;
+}
