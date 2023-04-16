7f38d8403c60e59fc13b7e790b92555fa73593d3
winetree94 <winetree94@gmail.com>
2023-04-16 15:59:21 +0900

    update

---
+++ 12-array/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("hello world!");
+
+  int arr[10] = {
+      [0] = 3,
+      [1] = 4,
+      [2] = 5,
+      [3] = 6,
+  };
+
+  int arr2[5] = {1, 2, 34, 56, 12};
+
+  printf("size of arr2: %lu", sizeof arr2);
+  printf("length of arr2: %lu", sizeof arr2 / sizeof arr2[0]);
+
+  int arr3[sizeof arr2];
+
+  return 0;
+}
